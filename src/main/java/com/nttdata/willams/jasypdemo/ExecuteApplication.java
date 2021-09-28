7f101af448f0e4343d98e186416b5f111dca010f
willams <willams.santos.sousa@everis.nttdata.com>
2021-09-28 20:52:02 -0300

    adicionou dependências principais

---
+++ src/main/java/com/nttdata/willams/jasypdemo/ExecuteApplication.java
@@ -0,0 +1,11 @@
+package com.nttdata.willams.jasypdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecuteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExecuteApplication.class, args);
+	}
+}
