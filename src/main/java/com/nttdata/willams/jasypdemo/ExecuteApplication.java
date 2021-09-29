3632d44880c3df42a07043357fa0caed718e5799
willams <willams.santos.sousa@everis.nttdata.com>
2021-09-28 21:54:32 -0300

    adicionou propriedade criptografada com jasypt

--- src/main/java/com/nttdata/willams/jasypdemo/ExecuteApplication.java
+++ src/main/java/com/nttdata/willams/jasypdemo/ExecuteApplication.java
@@ -1,10 +1,25 @@
 package com.nttdata.willams.jasypdemo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@EnableEncryptableProperties
+@Log4j2
 public class ExecuteApplication {
+	@Value("${minha.url.secreta}")
+	private String minhaUrlSecreta;
+
+	@PostConstruct
+	void show() {
+		log.info("### URL descriptografada: {}", minhaUrlSecreta);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExecuteApplication.class, args);
 	}
